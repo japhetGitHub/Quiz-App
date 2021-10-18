cf00e8087e280e7d22f54ad248108b28812456a8
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-10-18 14:45:53 -0400

    refactored the tables

--- db/schema/06_question_attemps.sql
+++ db/schema/06_question_attemps.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS question_scores CASCADE;
 CREATE TABLE question_scores (
   id SERIAL PRIMARY KEY NOT NULL,
   correct BOOLEAN,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  quiz_attempt_id INTEGER REFERENCES quiz_attempts(id) ON DELETE CASCADE
 );
