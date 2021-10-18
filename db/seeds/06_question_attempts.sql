a8779b9462d28db12a74be0ec8f43aeba74918a2
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-10-18 14:49:56 -0400

    refactored question attempts

--- db/seeds/06_question_attempts.sql
+++ db/seeds/06_question_attempts.sql
@@ -1,2 +1,5 @@
-INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (true, 2, 1),
-(false, 2, 1), (true, 2, 1), (false, 2, 1), (true, 2, 1);
+INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (true, 1, 1);
+INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (false, 2, 1);
+INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (true, 3, 1);
+INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (false, 4, 1);
+INSERT INTO question_attempts (correct, question_id, quiz_attempt_id) VALUES (true, 5, 1);
