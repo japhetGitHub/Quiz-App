4350ff5bb5c66ce4682ace9d8f5409051f100738
Japhet Elias <japhet9@gmail.com>
2021-10-20 18:17:12 -0400

    created routing endpoint for create quiz feature

--- server.js
+++ server.js
@@ -37,12 +37,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 // const usersRoutes = require("./routes/users");
 // const widgetsRoutes = require("./routes/widgets");
+const createQuizRoutes = require("./routes/create_quiz");
 
-// Mount all resource routes
+// Mount all resource routes000
+
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/users", usersRoutes(db));
 // app.use("/api/widgets", widgetsRoutes(db));
-
+app.use("/quiz/new", createQuizRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
