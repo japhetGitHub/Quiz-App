3657fad5ead10d5c028c6c421df07fc5e0797188
Japhet Elias <japhet9@gmail.com>
2021-10-21 16:51:22 -0400

    added login post req (via jquery) to front end

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
   $('#pageContent > form').submit(function(event) {
     event.preventDefault();
-    console.log("logged innn")
+    // $(this).serializeArray()[0].value
+    $.post( `/login`, {
+      user_id: $(this).serializeArray()[0].value
+    })
+
   });
 
 });
