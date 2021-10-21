35a95a4f51ecf913e88c34d12b45c1a655c78bf5
Japhet Elias <japhet9@gmail.com>
2021-10-21 16:53:26 -0400

    added cookie session middleware to server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -32,6 +33,12 @@ app.use(
 );
 
 app.use(express.static("public"));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['some secret key to encrypt the session value', 'another one to allow for key rotation'],
+}));
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
