4c1082dcce1219593cd49494ce42efba3ef0c003
Japhet Elias <japhet9@gmail.com>
2021-10-21 19:06:26 -0400

    fixed authentication handler logic

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,15 +1,20 @@
 module.exports = () => (req, res, next) => {
 
-  const whiteListRoutes = ['/login',];
+  const whiteListRoutes = ['/login'];
   const path = req.path;
 
-  if (!whiteListRoutes.includes(path)) { // if path is not /login
-    if(!req.session['user_id']) { // if user doesn't have a session
-      return res.redirect('/login'); // redirect invalid/logged-out user to /login
+  if (whiteListRoutes.includes(path)) { // if path is /login
+    if(req.session['user_id']) { // if user has a session
+      return res.redirect('/');
     } else {
       return next(); // if user has a session then let them through
     }
-  } else {
-    return next();
+  } else { // for every other path
+    if(req.session['user_id']) { // if user has a session
+      return next(); // let them through
+    } else {
+      return res.redirect('/login'); // if user doesn't have a session then don't let them through
+    }
   }
+
 };
